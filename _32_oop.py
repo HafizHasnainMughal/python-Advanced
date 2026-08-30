25bf8fb0d6e4c7f9247160082db8c726d835bfdf
HafizHasnainMughal <hafizmuhammadhasnain12@gmail.com>
2026-08-30 12:25:43 +0500

    opp

---
+++ _32_oop.py
@@ -0,0 +1,22 @@
+# OOP (Object-Oriented Programming)
+'''
+    Programming ka ek style jisme hum real-world entities ko
+    Objects aur Classes ki form mein represent karte hain.
+
+    In oop by using a class in which having many different
+    clid having their own properties.
+    In this case we  use class objects.
+
+'''
+
+print("----------------")
+print("simple oop structure ")
+class person:     # person is the class name
+    name="Mughal" # }
+    age=23        # } blue print
+    sex="male"    # }
+a=person()        # object created
+b=person()
+print(a.name)     #calling name form blue print
+print(a.age)
+print(a.sex)
