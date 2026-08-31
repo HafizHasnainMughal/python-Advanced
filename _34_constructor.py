2d2b27e828b72714ad3bd623340cfb730274f81b
HafizHasnainMughal <hafizmuhammadhasnain12@gmail.com>
2026-08-31 12:16:17 +0500

    add

--- _34_constructor.py
+++ _34_constructor.py
@@ -13,6 +13,17 @@ class student:
 first=student()
 
 print("---------------")
+class persn:
+    def __init__(self):
+        self.name='hassan'.title()
+        self.age=23
+        self.education="BS-Cs"
+    def display(self):
+        print(f"your name is {self.name} ")
+        print(f"your age is {self.age} ")
+        print(f"your education is {self.education} ")
+a=persn()
+a.display()
 
 
 
---
+++ _37_encapsulation.py
@@ -0,0 +1,13 @@
+# Encapsulation in class
+'''
+    Encapsulation Object-Oriented Programming (OOP) ka ek core concept hai,
+     jiska matlab hai Data (variables) aur Methods (functions) ko 
+     ek single unit (Class) mein pack kar dena aur outer access ko control karna.
+
+    Two main points:
+        Data Grouping: Related variables aur functions ko ek jagah rakhna.
+        Data Hiding (Protection): Sensitive data ko class ke bahar se
+          direct access ya modify hone se rokna.
+
+    
+'''
